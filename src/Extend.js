f858a5b4da3988dd5f150ce80496ba6550b246b1
Greg Cosgrove <gregcosgrove@gmail.com>
2014-05-30 11:12:38 -0500

    Implement extend and events

--- src/Extend.js
+++ src/Extend.js
@@ -1,2 +1,16 @@
-var extend = function() {
+// var extend = function(ChildClass,ParentClass) {
+//   ChildClass.prototype = new ParentClass ();
+//   ChildClass.prototype.constructor = ChildClass;
+// };
+
+var extend = function (destination, source) {
+  for (var property in source) {
+    destination[property] = source[property];
+  }
+  return destination;
 };
+
+// Should add properties from one object to another object
+// Should return the destination object
+// Should not set the source and destination object to equal each other
+// Should not add any additional properties to the source or destination
